432336571bfec5adc136446ce08b9711fe977541
Lisa Miner <midnightmoet@gmail.com>
2023-06-30 20:13:33 -0400

    JS completed

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-const btnEl = document.getElementById('btn');
-const animeContainerEl = document.querySelector('.anime-container');
-const animeImgEl = document.querySelector('.anime-img');
-const animeNameEl = document.querySelector('.anime-name');
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const amineNameEl = document.querySelector(".anime-name");
 
-const animeList = [
-
-    {
-        name: 'Naruto',
-        img: 'https://i.pinimg.com/originals/0a/0e/0a/0a0e0a1b0b0b0b0b0b0b0b0b0b0b0b0b.jpg'
-    },
-
-    {
-        name: 'One Piece',
-        img: 'https://i.pinimg.com/originals/0a/0e/0a/0a0e0a1b0b0b0b0b0b0b0b0b0b0b0b0b.jpg'
-    },
-
-    {
-        name: 'Bleach',
-        img: 'https://i.pinimg.com/originals/0a/0e/0a/0a0e0a1b0b0b0b0b0b0b0b0b0b0b0b0b.jpg'
-    },
-];
-
-btnEl.addEventListener('click', function(){
-    // console.log('hello'); // test
-    console.log(animeList[0].name); // test
+btnEl.addEventListener("click", async function () {
+  try {
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    amineNameEl.innerText = "Updating...";
+    animeImgEl.src = "spinner.svg";
+    const response = await fetch("https://api.catboys.com/img");
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    amineNameEl.innerText = data.artist;
+  } catch (error) {
+    console.log(error);
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    amineNameEl.innerText = "An error happened, please try again";
+  }
 });
